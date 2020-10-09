189332daa8e35d917300d7c8b36a8348b849ac45
kiarie <denniskiarie426@gmail.com>
2020-10-09 23:27:00 +0300

    make changes to page using scripts.js,show the other hidden paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $(".clickable").click(function() {
         $(".design").toggle();
     })
+    $(".clickable2").click(function() {
+        $(".development").toggle();
+    })
+    $(".clickable3").click(function() {
+        $(".product").toggle();
+    })
 })
