09bc5c14aa2482564a60d206cb19b5faafb8c06a
kiarie <denniskiarie426@gmail.com>
2020-10-12 01:49:15 +0300

    make changes to page using scripts.js and style.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
-$(document).ready(function() {
+ $(document).ready(function() {
     $(".clickable").click(function() {
-        $(".design").toggle();
+        $(".design").slideToggle();
     })
     $(".clickable2").click(function() {
-        $(".development").toggle();
+        $(".development").slideToggle();
     })
     $(".clickable3").click(function() {
-        $(".product").toggle();
+        $(".product").slideToggle();
     })
-    $("#contact form").submit(function(event) {
-        alert( " we have received your message")
-        event.preventDefault();
+    $(".floatcaption").hover(function(){
+        $(this).find(".caption").toggle(400);
+    })
+    $("form").submit(function(){
+        var name=document.getElementById("Name").value;
+        var email=document.getElementById("email").value;
+        var message=document.getElementById("message").value;
+        
+        if(name===""){
+            alert("please enter your name")
+        } else if (message===""){
+            alert("please enter your message")
+        } else if (email===""){
+            alert("please enter your email")
+        } else {
+            alert(name + "we have received your message successfully")
+        }
+    })
+    $("form#contact").onSubmit (function(){
+        $("form").each(function(){
+            this,reset();
+        })
     })
 })
